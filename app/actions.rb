6e5055513c82c3007b96db7984c2eb11084356d4
clmelissa <christinamelissa8@gmail.com>
2015-08-06 22:27:58 -0400

    debug-1

--- app/actions.rb
+++ app/actions.rb
@@ -15,8 +15,8 @@ end
 	
 #detail of the specific repair order
 get '/repair/:id' do
-	 @repair = Repair.find(params[:id])
-	 @user = User.find(@repair.user_id)
+	 #@repair = Repair.find(params[:id])
+	 #@user = User.find(@repair.user_id)
 	 #@comment = Update.where(repairs_id: params[:id])
 	 redirect '/'
 	 erb :repairDetail
