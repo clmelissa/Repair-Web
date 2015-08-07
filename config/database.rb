c93bf5687de4f1502079a2b843bfec6ff3d281be
clmelissa <christinamelissa8@gmail.com>
2015-08-06 22:24:39 -0400

    1

--- config/database.rb
+++ config/database.rb
@@ -4,12 +4,7 @@ configure do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  # Load all models from app/models, using autoload instead of require
-  # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
-  Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
-    filename = File.basename(model_file).gsub('.rb', '')
-    autoload ActiveSupport::Inflector.camelize(filename), model_file
-  end
+  
   
     if development?
       set :database, {
@@ -20,6 +15,11 @@ configure do
       set :database, ENV['DATABASE_URL']
     end
     
-
+    # Load all models from app/models, using autoload instead of require
+    # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
+    Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
+      filename = File.basename(model_file).gsub('.rb', '')
+      autoload ActiveSupport::Inflector.camelize(filename), model_file
+    end
   
 end
